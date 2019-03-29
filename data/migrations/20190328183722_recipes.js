6d326cf9c181578c6447eff0f8947eb78823b05d
Anthony Venturini <anthonydventurini@gmail.com>
2019-03-28 18:39:51 -0700

    adds recipes table using knex migrations

---
+++ data/migrations/20190328183722_recipes.js
@@ -0,0 +1,24 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('recipes', function(tbl) {
+        tbl
+            .increments();
+
+        tbl
+            .string('name', 128)
+            .notNullable()
+            .unique();
+
+        tbl
+            .integer('dishId')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('dishes')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('recipes');
+};
